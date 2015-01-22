796bbb87add564ac98ddd3c220e3bde2ca370a23
Lamb11 <abc_1118@hotmail.com>
2015-01-21 21:32:49 -0600

    Initial Commit

---
+++ Lab04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab04
+//
+//  Created by Alberto Cordero Arellanes on 21/01/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
