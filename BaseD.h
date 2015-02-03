85fb7771dcd62fdbd42dfb69ef1a1a754b98341c
Lamb11 <abc_1118@hotmail.com>
2015-02-02 23:24:42 -0600

    Añadi la libreria de SQL

---
+++ BaseD.h
@@ -0,0 +1,13 @@
+//
+//  BaseD.h
+//  Lab04
+//
+//  Created by Alberto Cordero Arellanes on 02/02/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseD : NSObject
+
+@end
---
+++ BaseD.m
@@ -0,0 +1,13 @@
+//
+//  BaseD.m
+//  Lab04
+//
+//  Created by Alberto Cordero Arellanes on 02/02/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import "BaseD.h"
+
+@implementation BaseD
+
+@end
